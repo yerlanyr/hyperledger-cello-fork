2b50dcea8252f65eb88451f7f648edbf0478203a
Yerlan Yar-Mukhamedov <e.yar-mukhamedov@alabs.team>
2021-09-14 16:18:50 +0600

    feat: logging to file handler

--- src/common/log.py
+++ src/common/log.py
@@ -6,7 +6,8 @@
 import os
 import logging
 
-log_handler = logging.StreamHandler()
+# log_handler = logging.StreamHandler()
+log_handler = logging.FileHandler('/root/cello.log')
 
 formatter = logging.Formatter("[%(asctime)s] %(levelname)s [%(name)s]"
                               " [%(filename)s:%(lineno)s %(funcName)20s()]"
